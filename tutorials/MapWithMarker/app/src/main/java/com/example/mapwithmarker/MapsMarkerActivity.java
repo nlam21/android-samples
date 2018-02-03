933e8b9ad9563528a919b8b3f4d178212f229abc
nlam21 <36091828+nlam21@users.noreply.github.com>
2018-02-03 10:39:11 -0500

    Update MapsMarkerActivity.java

--- tutorials/MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
+++ tutorials/MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
@@ -10,9 +10,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * An activity that displays a Google map with a marker (pin) to indicate a particular location.
- */
 public class MapsMarkerActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
@@ -28,15 +25,6 @@ public class MapsMarkerActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map when it's available.
-     * The API invokes this callback when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user receives a prompt to install
-     * Play services inside the SupportMapFragment. The API invokes this method after the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
